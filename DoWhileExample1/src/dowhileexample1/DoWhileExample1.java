e01d776ada5a36f8a9e258fb467f33113f801dff
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 17:10:26 +0100

    Add files via upload
    Updated

---
+++ DoWhileExample1/src/dowhileexample1/DoWhileExample1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dowhileexample1;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class DoWhileExample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i=1;
+        do{
+            System.out.println("Eat" +1 );
+            i++;}
+            while(i<=10);
+    }
+    
+}
